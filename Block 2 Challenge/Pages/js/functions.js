a9025f4a56f09e423558bb3293e5a78c41cdb37d
Mohammed Ahmed <2149869@talnet.nl>
2021-12-03 16:25:02 +0100

    Fixed JavaScript

--- Block 2 Challenge/Pages/js/functions.js
+++ Block 2 Challenge/Pages/js/functions.js
@@ -1,9 +1,9 @@
-function Hide(){
-  document.getElementById("thinkcode").style.display = "none";
-  document.getElementById("pro").style.display = "none";
-}
+function hide() {
+  var x = document.getElementById("projects");
 
-function Show(){
-  document.getElementById("thinkcode").style.display = "block";
-  document.getElementById("pro").style.display = "block";
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
