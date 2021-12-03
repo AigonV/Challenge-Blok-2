e6ed1708d49e27fe0607bad2e153aa2ddb46de47
Mohammed Ahmed <2149869@talnet.nl>
2021-12-03 14:02:21 +0100

    javascript

--- Block 2 Challenge/Pages/js/functions.js
+++ Block 2 Challenge/Pages/js/functions.js
@@ -1,3 +1,9 @@
-function uhh(){
-    alert("");
+function Hide(){
+  document.getElementById("thinkcode").style.display = "none";
+  document.getElementById("pro").style.display = "none";
+}
+
+function Show(){
+  document.getElementById("thinkcode").style.display = "block";
+  document.getElementById("pro").style.display = "block";
 }
